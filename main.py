f5a447b2b7f2f529131d955ccd99370fa0c4a159
SArcD <141703135+SArcD@users.noreply.github.com>
2025-07-10 15:41:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -263,104 +263,6 @@ elif opcion == "Proceso":
 
         # Expansor cpm el mapa de Abell 85
         with st.expander("🌌 Ver mapa interactivo del cúmulo Abell 85"):
-#            num_vars = ['Vel', 'Cl_d', '(u-g)', '(g-r)', '(r-i)', '(i-z)', 'Delta']
-#            cat_vars = ['M(parn)', 'Act']
-#            all_vars = num_vars + cat_vars
-
-#            selected_var = st.selectbox(
-#                "Variable para filtrar y colorear puntos:",
-#                options=all_vars
-#            )
-
-#            df_filtered = df.copy()
-
-#            # Filtrado según tipo
-#            if selected_var in num_vars:
-#                try:
-#                    df_filtered['range_label'] = pd.qcut(df_filtered[selected_var], 5, duplicates='drop')
-#                    labels = df_filtered['range_label'].cat.categories.astype(str).tolist()
-#                    #labels = fcluster(Z, t=num_clusters, criterion=criterion)
-                    #df.loc[data.index, 'Subcluster'] = labels
-#                    selected_labels = st.multiselect(
-#                        "Selecciona uno o varios rangos:",
-#                        options=labels,
-#                        default=labels
-#                    )
-#                    df_filtered = df_filtered[df_filtered['range_label'].astype(str).isin(selected_labels)]
-#                except Exception as e:
-#                    st.warning(f"No se pudieron crear rangos para esta variable. Detalle: {e}")
-#            elif selected_var in cat_vars:
-#                labels = df_filtered[selected_var].dropna().unique().tolist()
-#                selected_labels = st.multiselect(
-#                    "Selecciona una o varias categorías:",
-#                    options=labels,
-#                    default=labels
-#                )
-#                df_filtered = df_filtered[df_filtered[selected_var].isin(selected_labels)]
-
-#            # Hover enriquecido
-#            hover_data = {
-#                "RA": True, "Dec": True,
-#                "Vel": True, "Cl_d": True, "Delta": True,
-#                "(u-g)": True, "(g-r)": True, "(r-i)": True, "(i-z)": True,
-#                "M(IP)": True, "M(ave)": True, "Act": True
-#            }
-
-#            # Columnas necesarias para asegurar que existan
-#            required_cols = {'RA', 'Dec', 'ID'} | set(hover_data.keys())
-
-#            if required_cols.issubset(df.columns):
-#                fig = px.scatter(
-#                    df_filtered,
-#                    x="RA",
-#                    y="Dec",
-#                    color=selected_var,
-#                    hover_name="ID",
-#                    hover_data=hover_data,
-#                    title=f"Mapa filtrado por: {selected_var}"
-#                )
-
-#                fig.add_trace(
-#                    go.Histogram2dContour(
-#                        x=df_filtered['RA'],
-#                        y=df_filtered['Dec'],
-#                        ncontours=10,
-#                        colorscale='Viridis',
-#                        contours_coloring='lines',
-#                        line_width=2,
-#                        opacity=0.5,
-#                        showscale=False,
-#                        hoverinfo='skip'
-#                    )
-#                )
-
-#                fig.update_yaxes(autorange="reversed")
-#                fig.update_layout(
-#                    xaxis_title="Ascensión Recta (RA, grados)",
-#                    yaxis_title="Declinación (Dec, grados)",
-#                    height=700,
-#                    width=900
-#                )
-#                fig.update_xaxes(autorange="reversed")
-
-            
-#                st.plotly_chart(fig)
-
-#                # Botón para descargar tabla filtrada
-#                st.download_button(
-#                    "💾 Descargar tabla filtrada",
-#                    df_filtered.to_csv(index=False).encode('utf-8'),
-#                    file_name="galaxias_filtradas.csv",
-#                    mime="text/csv"
-#                )
-#            else:
-#                st.warning(
-#                    f"Faltan columnas necesarias para el mapa interactivo: "
-#                    f"{required_cols - set(df.columns)}"
-#                )
-
-
-
             import plotly.express as px
             import plotly.graph_objects as go
             import pandas as pd
@@ -433,7 +335,7 @@ elif opcion == "Proceso":
                         x=df_filtered['RA'],
                         y=df_filtered['Dec'],
                         ncontours=10,
-                        colorscale='plasma',
+                        colorscale='viridis',
                         contours_coloring='lines',
                         line_width=2,
                         opacity=0.5,
@@ -492,7 +394,7 @@ elif opcion == "Proceso":
                     yaxis_title="Declinación (Dec, grados)",
                     height=700,
                     width=900,
-                    plot_bgcolor='black',
+                    plot_bgcolor='blue',
                     paper_bgcolor='black',
                     font=dict(color="white")
                 )

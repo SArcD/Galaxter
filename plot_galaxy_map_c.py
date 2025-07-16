e1ec4f8d282ccb212fb0afb33aa13477e237f513
SArcD <141703135+SArcD@users.noreply.github.com>
2025-07-16 17:02:51 -0600

    Update plot_galaxy_map_c.py

--- plot_galaxy_map_c.py
+++ plot_galaxy_map_c.py
@@ -104,7 +104,9 @@ def plot_galaxy_map_with_distances(df, ra_col='RA', dec_col='Dec', morph_col='M(
     # Ajustes dinámicos
     # -------------------------------------------------------
     st.header("Simulación visual del cúmulo (con distancia)")
-    show_stars = st.sidebar.checkbox("Mostrar estrellas de campo", value=True)
+    #show_stars = st.sidebar.checkbox("Mostrar estrellas de campo", value=True)
+    show_stars = st.sidebar.checkbox("Mostrar estrellas de campo", value=True, key=f"{widget_prefix}_stars")
+
     morph_filter = st.sidebar.multiselect("Filtrar morfología", sorted(df[morph_col].dropna().unique()),
                                           default=sorted(df[morph_col].dropna().unique()))
     df_filtered = df[df[morph_col].isin(morph_filter)].dropna(subset=[ra_col, dec_col, morph_col, rf_col])

dba2fea129f60d54160d2ccbc477a6627535a314
SArcD <141703135+SArcD@users.noreply.github.com>
2025-07-16 16:59:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2872,7 +2872,7 @@ En esta sección puede colocar el nombre de cualquiera de las columnas de la bas
             df_with_dist = calculate_comoving_distance(df)
 
             from plot_galaxy_map_c import plot_galaxy_map_with_distances
-            img = plot_galaxy_map_c(df_with_dist)
+            img = plot_galaxy_map_with_distances(df)
             img.save("mapa_galaxias_corregido.png")
             img.show()
 
